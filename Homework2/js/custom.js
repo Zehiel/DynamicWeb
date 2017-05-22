4d220a60fb5160e566d9b31c4553e684cd0d5560
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-05-22 20:36:23 +0200

    Added missing regexes and password meter

--- Homework2/js/custom.js
+++ Homework2/js/custom.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+
+    var strength = {
+        0: "Worst",
+        1: "Bad",
+        2: "Weak",
+        3: "Good",
+        4: "Strong"
+    }
+
     var navListItems = $('ul.setup-panel li a'),
         allWells = $('.setup-content');
 
@@ -23,27 +32,20 @@ $(document).ready(function() {
 
     // DEMO ONLY //
     $('#activate-step-2').on('click', function(e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-        $(this).remove();
+            $('ul.setup-panel li:eq(1)').removeClass('disabled');
+            $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+            $(this).remove();
+
+
     })
 
     $('#activate-step-3').on('click', function(e) {
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        $(this).remove();
-    })
 
-    $('#activate-step-4').on('click', function(e) {
-        $('ul.setup-panel li:eq(3)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-4"]').trigger('click');
-        $(this).remove();
-    })
+            $('ul.setup-panel li:eq(2)').removeClass('disabled');
+            $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+            $(this).remove();
 
-    $('#activate-step-3').on('click', function(e) {
-        $('ul.setup-panel li:eq(2)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
-        $(this).remove();
+
     })
 
     $('#nameinput').keyup(function (event) {
@@ -102,6 +104,78 @@ $(document).ready(function() {
 
         }
     });
+
+    $('#postalcodeinput').keyup(function () {
+        var value = $(this).val().split("-").join(""); // remove hyphens
+        if (value.length > 0) {
+            value = value.match(new RegExp('[0-9]{1,3}', 'g')).join("-");
+            if(value.length==6) {
+                document.getElementById("postalcode").style.background = "green"
+            }else {
+                document.getElementById("postalcode").style.background = "red"
+            }
+        }
+        $(this).val(value);
+    });
+
+    $('#emailinput').keyup(function (event) {
+
+        var value = $(this).val();
+        if (value.length > 0) {
+            var patt =  new  RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            if(patt.test(value)){
+                document.getElementById("email").style.background = "green"
+            }else {
+                document.getElementById("email").style.background = "red"
+            }
+
+        }
+    });
+
+    $('#ipinput').keyup(function (event) {
+
+        var value = $(this).val();
+        if (value.length > 0) {
+            var patt =  new  RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
+            if(patt.test(value)){
+                document.getElementById("ip").style.background = "green"
+            }else {
+                document.getElementById("ip").style.background = "red"
+            }
+
+        }
+    });
+
+    $('#urlinput').keyup(function (event) {
+
+        var value = $(this).val();
+        if (value.length > 0) {
+            var patt =  new  RegExp(/(http|ftp|https|file):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~‌​+#-]*[\w@?^=%&amp;\/‌​~+#-])?/);
+            if(patt.test(value)){
+                document.getElementById("url").style.background = "green"
+            }else {
+                document.getElementById("url").style.background = "red"
+            }
+
+        }
+    });
+
+    $('#passwordinput').keyup(function (event) {
+        var value = $(this).val();
+        var result = zxcvbn(value);
+        console.log(result);
+        // Update the password strength meter
+        document.getElementById('password-strength-meter').value = result.score;
+
+        // Update the text indicator
+        if (value !== "") {
+            document.getElementById("password-strength-text")
+            document.getElementById("password").style.background = "green"
+        } else {
+            document.getElementById("password").style.background = "red"
+        }
+    });
+
 });
 
 
@@ -204,6 +278,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
         console.log(counter);
     });
 });
+
+
+
 
 
 
